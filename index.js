e88935a53a5b3e0af47af75d3657a8f869c94daf
Mei Nghiem-Lin <mcnghiem@gmail.com>
2021-11-05 02:16:00 +0000

    add fetchCoordsByIP function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // const { fetchMyIP } = require('./iss');
 
-// fetchMyIP((error, ip) => {
+const { fetchCoordsByIP } = require("./iss");
+
+// // fetchMyIP((error, ip) => {
 //   if (error) {
 //     console.log("It didn't work!" , error);
 //     return;
@@ -8,3 +10,12 @@
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+fetchCoordsByIP ('174.119.235.1'), (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+};
